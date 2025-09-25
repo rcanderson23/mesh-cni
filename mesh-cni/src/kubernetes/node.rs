9ea2f1e12269cba0270c4421321ec3d266324a87
Carson Anderson <rcanderson23@gmail.com>
2025-09-25 15:35:34 -0600

    cilium taint removal

--- mesh-cni/src/kubernetes/node.rs
+++ mesh-cni/src/kubernetes/node.rs
@@ -4,13 +4,14 @@ use kube::api::PostParams;
 use kube::{Api, ResourceExt};
 
 const TAINT_MESH_STARTUP: &str = "mesh-cni.dev/startup";
+const TAINT_CILIUM_STARTUP: &str = "node.cilium.io/agent-not-ready";
 pub async fn remove_startup_taint(client: kube::Client, node_name: String) -> Result<()> {
     let node_api: Api<Node> = Api::all(client);
     let mut this_node = node_api.get(&node_name).await?;
     if let Some(ref mut spec) = this_node.spec
         && let Some(ref mut taints) = spec.taints
     {
-        taints.retain(|t| t.key != TAINT_MESH_STARTUP);
+        taints.retain(|t| t.key != TAINT_MESH_STARTUP || t.key != TAINT_CILIUM_STARTUP);
         spec.taints = Some(taints.to_vec());
         node_api
             .replace(&this_node.name_any(), &PostParams::default(), &this_node)
